a988864c9d49db2cd4566f849a366f0f359972f4
Rory Dowse <rorydowse@hotmail.com>
2024-08-06 14:46:15 -0700

    update historyService.ts

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,5 +1,12 @@
 // TODO: Define a City class with name and id properties
-
+class City {
+  name: string;
+  id: string;
+  constructor(name: string, id: string) {
+    this.name = name;
+    this.id = id;
+  }
+}
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
@@ -15,10 +22,24 @@ class HistoryService {
   }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
   // async getCities() {}
+  async getCities(): Promise<City[]> {
+    const cities = await this.read();
+    return cities;
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   // async addCity(city: string) {}
+  async addCity(city: string): Promise<void> {
+    const cities = await this.read();
+    cities.push({ name: city, id: Date.now().toString() });
+    await this.write(cities);
+  }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
+  async removeCity(id: string): Promise<void> {
+    const cities = await this.read();
+    const updatedCities = cities.filter((city) => city.id !== id);
+    await this.write(updatedCities);
+  }
 }
 
 export default new HistoryService();
